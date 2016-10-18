e9002849840dab48deeb3c5c1c9e3bf18f173977
ItamarGronich <ItamarGronich@Gmail.com>
2016-10-18 23:02:35 +0300

    Added Default layout.

--- src/elm/App/View.elm
+++ src/elm/App/View.elm
@@ -2,13 +2,11 @@ module App.View exposing (..)
 
 import App.Model exposing (..)
 import App.Update exposing (..)
-import Html exposing (h1, text, ul, li, div, button)
-import Menu.View exposing (menu)
+import Html exposing (body)
+import Layouts.Default.View as DefaultLayout exposing (view)
 
 
 view : Model -> Html.Html Msg
 view model =
-    div []
-        [ (menu model)
-        , h1 [] [ text (toString model.activePage) ]
-        ]
+    body []
+        (DefaultLayout.view model)
